f958a15b15fb5ca6c84f7e8a9428b5406abbb692
sophie <zvava@disroot.org>
2022-04-28 00:13:23 +0100

    terminology in todo

--- @Resources/compiler/compiler.js
+++ @Resources/compiler/compiler.js
@@ -1,6 +1,6 @@
 console.log("- reading files...");
 
 // fs.readDir or something
-// read all of the files and create some objects
+// parse all of the scripts and create some objects
 // read all of the css and apply colors, etc. to corrosponding objects
 // export all applications into .ini files
